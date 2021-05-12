5762fb18034500a3b99095a5d0f2cbd2586d2056
Marcel <marcel.mikolas@gmail.com>
2021-05-12 18:58:56 +0200

    App

--- client/src/app/jonanek/jonanek.component.ts
+++ client/src/app/jonanek/jonanek.component.ts
@@ -1,86 +1,61 @@
 import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Inject, InjectionToken, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'napicu-jonanek',
   templateUrl: './jonanek.component.html',
   styleUrls: ['./jonanek.component.scss']
 })
+
 export class JonanekComponent implements OnInit {
-  protected Counter = window.localStorage.getItem("counter");
-  count: number = 0;
-  Clicked: boolean = false;
+  protected Counter: number = window.localStorage.getItem("counter") as any | 0;
+  protected Clicked: boolean = false;
   protected Jonanek1: any;
   protected Jonanek2: any;
+  SessionCounter: number = 0;
+  count: number = this.Counter | 0;
 
 
 
 
+  constructor(@Inject(DOCUMENT) private doc: Document) { }
 
-  constructor(@Inject(DOCUMENT) private doc: Document) {     
-  }
-
-
-  
   ngOnInit(): void {
     this.Jonanek1 = this.doc.getElementById("Jonanek1");
     this.Jonanek2 = this.doc.getElementById("Jonanek2");
 
-    
-    
-  }
-  
-  ngAfterViewInit() {
 
-    console.log(this.Jonanek1);
-    console.log(this.Jonanek2);
-    
+
+  }
+
+  ngAfterViewInit() {
     window.addEventListener('keydown', this.Click);
     this.Jonanek1.addEventListener('touchstart', this.Click);
     this.Jonanek2.addEventListener('touchstart', this.Click);
     this.Jonanek1.addEventListener('mousedown', this.Click);
     this.Jonanek2.addEventListener('mousedown', this.Click);
+
+    window.addEventListener('keyup', this.ClickNormal);
     this.Jonanek1.addEventListener('mouseup', this.ClickNormal);
     this.Jonanek1.addEventListener('touchend', this.ClickNormal);
     this.Jonanek2.addEventListener('mouseup', this.ClickNormal);
     this.Jonanek2.addEventListener('touchend', this.ClickNormal);
-    window.addEventListener('keyup', this.ClickNormal);
-    
-    
-  }
-  
-  
-  
-  Click(e: Event): void {
-      //e.preventDefault();
-    this.Jonanek1 = document.getElementById("Jonanek1");
-    this.Jonanek2 = document.getElementById("Jonanek2");
-    var x = document.getElementById("Jonanek1");
 
-    if (this.Clicked)
-      return; this.Clicked = true; this.count++;
-    this.count++;
+  }
+  protected Click = (e: Event): void => {
+    if (this.Clicked) return; this.Clicked = true; this.count++
+    this.SessionCounter++;
     this.Jonanek1.classList.replace("block", "none");
     this.Jonanek2.classList.replace("none", "block");
-
-
-    // this.click.innerHTML = Counter;
-
-    // JonanekSound.cloneNode(true).play();
-
-    //window.localStorage.setItem("counter", this.count.toString());
   }
 
-
-  ClickNormal(e: Event): void {
-    //e.preventDefault();
-
-
-    this.Jonanek1 = document.getElementById("Jonanek1");
-    this.Jonanek2 = document.getElementById("Jonanek2");
+  protected ClickNormal = (e: Event): void => {
     this.Clicked = false;
     this.Jonanek1.classList.replace("none", "block");
     this.Jonanek2.classList.replace("block", "none");
-
   }
+
+
 }
