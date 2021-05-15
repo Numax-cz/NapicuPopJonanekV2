4908c7cc79c85c8739aead6502b23bfc775319ce
Marcel <marcel.mikolas@gmail.com>
2021-05-15 21:58:57 +0200

    App

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
+
+import { AppComponent } from "./app.component";
 import { JonanekComponent } from './jonanek/jonanek.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { ShopComponent } from './shop/shop.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ErrorComponent } from './error/error.component';
+
+const routes: Routes = [
+  { path: '', component: JonanekComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: '**', component: ErrorComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     JonanekComponent,
     ShopComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -10,8 +10,67 @@ export class ShopComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
+
   }
-  
+
+  Play(e: any): void {
+    console.log(e.sound);
+    var sound = new Audio(e.sound);
+    sound.play();
+  }
+
+  items = [
+    {
+      title: "Vole",
+      cena: 1000,
+      sound: "/assets/sounds/vole.wav"
+    },
+    {
+      title: "Nemam",
+      cena: 2000,
+      sound: "/assets/sounds/nema.wav"
+
+    },
+    {
+      title: "Zdrave",
+      cena: 4500,
+      sound: "/assets/sounds/zdrave.wav"
+    },
+    {
+      title: "Síla",
+      cena: 6422,
+      sound: "/assets/sounds/sila.wav"
+
+    },
+    {
+      title: "AAAAAAA",
+      cena: 7000,
+      sound: "/assets/sounds/AAAAAAA.wav"
+
+    },
+    {
+      title: "Husté",
+      cena: 8500,
+      sound: "/assets/sounds/huste.wav"
+
+    },
+    {
+      title: "MMA",
+      cena: 13000,
+      sound: "/assets/sounds/mma.wav"
+
+    },
+    {
+      title: "Plecháč",
+      cena: 50000,
+      sound: "/assets/sounds/plechac.wav"
+
+    },
+    {
+      title: "Picus123",
+      cena: 100000,
+      sound: "/assets/sounds/Picus123.wav"
+    },
+  ]
 
 }
