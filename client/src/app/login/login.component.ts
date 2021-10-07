f0eef9cf69f14110aaac8eb038ab82549ec8fec5
Marcel <numax.cz@protonmail.ch>
2021-10-07 16:52:32 +0200

    Upraven login, přidáno post

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,6 +1,18 @@
 import { DOCUMENT } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { Form } from '@angular/forms';
+
+declare interface ApiData {
+  username: string;
+  email: string;
+  heslo: string;
+  reheslo: string;
+}
+declare interface ApiLoginRegisterR {
+  code: number;
+  msg: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -13,16 +25,47 @@ export class LoginComponent implements OnInit, AfterViewInit {
   @ViewChild('Pass1') public declare Pass1: any;
   @ViewChild('Pass2') public declare Pass2: any;
 
-  public declare static UserData: any;
-  constructor() {}
+
+
+
+
+  constructor(protected http: HttpClient) {}
+
   ngOnInit(): void {}
 
-  ngAfterViewInit(): void { }
-  
+  ngAfterViewInit(): void {
+   
+  }
 
+  public checkUserName(): void {
+    this.http
+      .post<any>('/api/usernamecheck', this.getValue(this.Name))
+      .subscribe((data: ApiLoginRegisterR) => {
+        if (data.code == 0) {
+          this.unsetBackground(this.Name);
+        } else {
+          this.setBackground(this.Name);
+        }
+      });
+  }
 
-  public onSubmit(data: any): void {
-    console.log(data);
-    
+  protected setBackground(element: any): void {
+    element.nativeElement.style.background = 'rgb(235, 47, 6)';
+  }
+  protected unsetBackground(element: any): void {
+    element.nativeElement.style.background = null;
+  }
+
+  protected getValue(value: any): string {
+    return value.nativeElement.value;
+  }
+
+  public onSubmit(data: ApiData): void {
+    if (!data) return;
+    this.http.post<any>('/api/reg', data).subscribe((data: ApiLoginRegisterR) => {
+      if (data.code == 0) {
+      } else {
+      }
+    });
   }
 }
