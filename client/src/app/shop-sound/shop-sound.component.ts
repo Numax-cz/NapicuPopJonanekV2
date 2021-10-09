a03b469c43ba19aeaf4d65ad0101179873b97279
Marcel <numax.cz@protonmail.ch>
2021-10-09 11:03:21 +0200

    Přidané zvuky do shopu

--- client/src/app/shop-sound/shop-sound.component.ts
+++ client/src/app/shop-sound/shop-sound.component.ts
@@ -4,22 +4,19 @@ import { JonanekComponent } from '../jonanek/jonanek.component';
 import { ShopComponent } from '../shop/shop.component';
 
 export interface ArrayListSound {
-  title: string,
-  cena: string | number,
-  sound: string,
-  owned: boolean,
-  CBuy: boolean
+  title: string;
+  cena: string | number;
+  sound: string;
+  owned: boolean;
+  CBuy: boolean;
 }
 @Component({
   selector: 'app-shop-sound',
   templateUrl: './shop-sound.component.html',
-  styleUrls: ['./shop-sound.component.scss']
+  styleUrls: ['./shop-sound.component.scss'],
 })
-
-
 export class ShopSoundComponent implements OnInit {
-
-  img: string = "assets/sound.png";
+  img: string = 'assets/sound.png';
   btn: boolean = true;
 
   constructor() {
@@ -34,15 +31,12 @@ export class ShopSoundComponent implements OnInit {
     this.Check();
   }
 
-
   protected Check(): void {
-
-
     this.items.forEach((e: ArrayListSound) => {
       JSON.parse(ShopComponent.OwnedSounds).forEach((i: string) => {
         if (e.title === i) {
           e.owned = true;
-          e.cena = "Vybrat";
+          e.cena = 'Vybrat';
           return;
         }
         if (e.cena >= JonanekComponent.count && !e.owned) {
@@ -68,17 +62,15 @@ export class ShopSoundComponent implements OnInit {
   protected SelectSong(i: ArrayListSound) {
     this.SelectSongRename(i);
     JonanekComponent.song = i.sound;
-    window.localStorage.setItem("song", i.sound);
+    window.localStorage.setItem('song', i.sound);
     JonanekComponent.LoadSound();
     ShopComponent.PlaySelectSound();
     this.Check();
   }
 
   protected SelectSongRename(i: ArrayListSound) {
-    i.cena = "Vybraný";
+    i.cena = 'Vybraný';
   }
-
-
 
   public BuySong(i: ArrayListSound): void {
     if (i.CBuy && !i.owned) {
@@ -94,141 +86,146 @@ export class ShopSoundComponent implements OnInit {
     ShopComponent.BuySound(i);
   }
 
-
   items: ArrayListSound[] = [
     {
-      title: "Smích",
+      title: 'Smích',
       cena: 0,
-      sound: "/assets/sounds/nevim.wav",
+      sound: '/assets/sounds/nevim.wav',
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "Pupek1",
+      title: 'Pupek1',
       cena: 50,
-      sound: "/assets/sounds/pupek2.wav",
+      sound: '/assets/sounds/pupek2.wav',
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "Bum!!",
+      title: 'Bum!!',
       cena: 80,
-      sound: "/assets/sounds/bum.wav",
+      sound: '/assets/sounds/bum.wav',
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "Vole",
+      title: 'Vole',
       cena: 100,
-      sound: "/assets/sounds/vole.wav",
+      sound: '/assets/sounds/vole.wav',
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "Pupek2",
+      title: 'Pupek2',
       cena: 150,
-      sound: "/assets/sounds/pupek3.wav",
+      sound: '/assets/sounds/pupek3.wav',
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "Ha",
+      title: 'Ha',
       cena: 170,
-      sound: "/assets/sounds/ha.wav",
+      sound: '/assets/sounds/ha.wav',
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "Hahaha",
-      cena: 580,
-      sound: "/assets/sounds/hahaha.wav",
-      owned: false,
-      CBuy: false
-    },
-    {
-      title: "Nemam",
+      title: 'Nemam',
       cena: 200,
-      sound: "/assets/sounds/nema.wav",
+      sound: '/assets/sounds/nema.wav',
       owned: false,
-      CBuy: false
-
+      CBuy: false,
     },
     {
-      title: "Zdrave",
+      title: 'Zdrave',
       cena: 450,
-      sound: "/assets/sounds/zdrave.wav",
+      sound: '/assets/sounds/zdrave.wav',
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "Hmmm",
+      title: 'Hmmm',
       cena: 500,
-      sound: "/assets/sounds/hm.wav",
+      sound: '/assets/sounds/hm.wav',
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "Síla",
+      title: 'Hahaha',
+      cena: 580,
+      sound: '/assets/sounds/hahaha.wav',
+      owned: false,
+      CBuy: false,
+    },
+    {
+      title: 'Síla',
       cena: 642,
-      sound: "/assets/sounds/sila.wav",
+      sound: '/assets/sounds/sila.wav',
       owned: false,
-      CBuy: false
-
+      CBuy: false,
     },
     {
-      title: "AAAAAAA",
+      title: 'AAAAAAA',
       cena: 700,
-      sound: "/assets/sounds/AAAAAAA.wav",
+      sound: '/assets/sounds/AAAAAAA.wav',
       owned: false,
-      CBuy: false
-
+      CBuy: false,
     },
     {
-      title: "Nechtěl bych",
+      title: 'Nechtěl bych',
       cena: 725,
-      sound: "/assets/sounds/nechtelbych.wav",
+      sound: '/assets/sounds/nechtelbych.wav',
       owned: false,
-      CBuy: false
-
+      CBuy: false,
     },
     {
-      title: "Husté",
+      title: 'Husté',
       cena: 850,
-      sound: "/assets/sounds/huste.wav",
+      sound: '/assets/sounds/huste.wav',
       owned: false,
-      CBuy: false
-
+      CBuy: false,
     },
     {
-      title: "MMA",
+      title: 'Se mi laguje telefon',
+      cena: 880,
+      sound: '/assets/sounds/semilagujetelefon.wav.wav',
+      owned: false,
+      CBuy: false,
+    },
+    {
+      title: 'MMA',
       cena: 900,
-      sound: "/assets/sounds/mma.wav",
+      sound: '/assets/sounds/mma.wav',
       owned: false,
-      CBuy: false
-
+      CBuy: false,
     },
     {
-      title: "Plecháč",
+      title: 'Plecháč',
       cena: 1000,
-      sound: "/assets/sounds/plechac.wav",
+      sound: '/assets/sounds/plechac.wav',
       owned: false,
-      CBuy: false
-
+      CBuy: false,
     },
     {
-      title: "Picus123",
+      title: 'Picus123',
       cena: 2000,
-      sound: "/assets/sounds/Picus123.wav",
+      sound: '/assets/sounds/Picus123.wav',
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "Pupek3",
-      cena: 2500,
-      sound: "/assets/sounds/pupek1.wav",
+      title: 'Picus123456789',
+      cena: 2000,
+      sound: '/assets/sounds/picus123456789.wav',
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
-
-  ]
+    {
+      title: 'Pupek3',
+      cena: 2500,
+      sound: '/assets/sounds/pupek1.wav',
+      owned: false,
+      CBuy: false,
+    },
+  ];
 }
