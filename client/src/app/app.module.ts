c44641547e3e22c04cee9a1061fb3bd5ac907cf7
Marcel <numax.cz@protonmail.ch>
2021-10-10 16:57:28 +0200

    Shop přejmenován na menu

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
     component: JonanekComponent,
     children: [
       {
-        path: 'shop',
+        path: 'menu',
         component: ShopComponent,
         children: [
           { path: 'sound', component: ShopSoundComponent },
