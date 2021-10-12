a8c746f24f1a919dbb4f83e1d65cca90db5877db
Marcel <numax.cz@protonmail.ch>
2021-10-12 18:22:06 +0200

    Přidány pozadí do obchodu

--- client/src/app/shop-background/shop-background.component.ts
+++ client/src/app/shop-background/shop-background.component.ts
@@ -2,30 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { JonanekComponent } from '../jonanek/jonanek.component';
 import { ShopComponent } from '../shop/shop.component';
 
-
 interface ArrayImg {
-  img1: string,
-  img2: string
+  img1: string;
+  img2: string;
 }
-
 
 export interface ArrayListImg {
-  title: string,
-  cena: string | number,
-  imgs: ArrayImg[],
-  owned: boolean,
-  CBuy: boolean
+  title: string;
+  cena: string | number;
+  imgs: ArrayImg[];
+  owned: boolean;
+  CBuy: boolean;
 }
-
 
 @Component({
   selector: 'app-shop-background',
   templateUrl: './shop-background.component.html',
-  styleUrls: ['./shop-background.component.scss']
+  styleUrls: ['./shop-background.component.scss'],
 })
 export class ShopBackgroundComponent implements OnInit {
-
-
   btn: boolean = true;
 
   constructor() {
@@ -40,15 +35,12 @@ export class ShopBackgroundComponent implements OnInit {
     this.Check();
   }
 
-
-
   protected Check(): void {
     this.items.forEach((e: ArrayListImg) => {
       JSON.parse(ShopComponent.OwnedBackground).forEach((i: string) => {
-
         if (e.title === i) {
           e.owned = true;
-          e.cena = "Vybrat";
+          e.cena = 'Vybrat';
           return;
         }
         if (e.cena >= JonanekComponent.count && !e.owned) {
@@ -62,7 +54,6 @@ export class ShopBackgroundComponent implements OnInit {
     });
   }
 
-
   get Background(): string {
     return JonanekComponent.background1;
   }
@@ -75,7 +66,7 @@ export class ShopBackgroundComponent implements OnInit {
     JonanekComponent.background2 = img2;
     var list = JSON.stringify([img1, img2]);
 
-    window.localStorage.setItem("background", list);
+    window.localStorage.setItem('background', list);
     JonanekComponent.LoadBackground();
     ShopComponent.PlaySelectSound();
 
@@ -83,10 +74,8 @@ export class ShopBackgroundComponent implements OnInit {
   }
 
   protected SelectBackgroundRename(i: ArrayListImg) {
-    i.cena = "Vybraný";
+    i.cena = 'Vybraný';
   }
-
-
 
   public BuyBackground(i: ArrayListImg): void {
     if (i.CBuy && !i.owned) {
@@ -101,54 +90,105 @@ export class ShopBackgroundComponent implements OnInit {
     ShopComponent.BuyBackground(i);
   }
 
-
   items: ArrayListImg[] = [
     {
-      title: "Smích",
+      title: 'Smích',
       cena: 0,
-      imgs: [{ img1: "/assets/Jonanek1.webp", img2: "/assets/Jonanek2.webp" }],
+      imgs: [{ img1: '/assets/Jonanek1.webp', img2: '/assets/Jonanek2.webp' }],
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "Smile",
+      title: 'Smile',
       cena: 50,
-      imgs: [{ img1: "/assets/JonanekSmile1.webp", img2: "/assets/JonanekSmile2.webp" }],
+      imgs: [{ img1: '/assets/JonanekSmile1.webp', img2: '/assets/JonanekSmile2.webp' }],
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
-
-
-
-
     {
-      title: "JonanekThinking",
+      title: 'JonanekThinking',
       cena: 100,
-      imgs: [{ img1: "/assets/Thinking1.webp", img2: "/assets/Thinking2.webp" }],
+      imgs: [{ img1: '/assets/Thinking1.webp', img2: '/assets/Thinking2.webp' }],
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "JonanekVenku",
+      title: 'JonanekVenku',
       cena: 150,
-      imgs: [{ img1: "/assets/JonanekVenku1.webp", img2: "/assets/JonanekVenku2.webp" }],
+      imgs: [{ img1: '/assets/JonanekVenku1.webp', img2: '/assets/JonanekVenku2.webp' }],
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "JonanekSpi",
+      title: 'JonanekSpi',
       cena: 200,
-      imgs: [{ img1: "/assets/JonanekSpi1.webp", img2: "/assets/JonanekSpi2.webp" }],
+      imgs: [{ img1: '/assets/JonanekSpi1.webp', img2: '/assets/JonanekSpi2.webp' }],
       owned: false,
-      CBuy: false
+      CBuy: false,
     },
     {
-      title: "JonanekBirell",
+      title: 'JonanekBirell',
       cena: 350,
-      imgs: [{ img1: "/assets/JonanekLavice1.webp", img2: "/assets/JonanekLavice2.webp" }],
+      imgs: [{ img1: '/assets/JonanekLavice1.webp', img2: '/assets/JonanekLavice2.webp' }],
       owned: false,
-      CBuy: false
+      CBuy: false,
+    },
+    {
+      title: 'JonanekBirell2',
+      cena: 450,
+      imgs: [{ img1: '/assets/BirellNaLavicceLol1.webp', img2: '/assets/BirellNaLavicceLol2.webp' }],
+      owned: false,
+      CBuy: false,
+    },
+    {
+      title: 'JonanekBirell Hvězdárna',
+      cena: 500,
+      imgs: [{ img1: '/assets/BirellUHvezdarny1.webp', img2: '/assets/BirellUHvezdarny2.webp' }],
+      owned: false,
+      CBuy: false,
+    },
+    {
+      title: 'Invaze Plechu',
+      cena: 600,
+      imgs: [{ img1: '/assets/InvazePlechu1.webp', img2: '/assets/InvazePlechu2.webp' }],
+      owned: false,
+      CBuy: false,
     },
 
-  ]
+    {
+      title: 'UFO',
+      cena: 750,
+      imgs: [{ img1: '/assets/KerUfo1.webp', img2: '/assets/KerUfo2.webp' }],
+      owned: false,
+      CBuy: false,
+    },
+    {
+      title: 'WC',
+      cena: 900,
+      imgs: [{ img1: '/assets/OranzovyHajzly1.webp', img2: '/assets/OranzovyHajzly2.webp' }],
+      owned: false,
+      CBuy: false,
+    },
+    {
+      title: 'WC tělocvična',
+      cena: 1000,
+      imgs: [{ img1: '/assets/HajzlUTelocviny1.webp', img2: '/assets/HajzlUTelocviny2.webp' }],
+      owned: false,
+      CBuy: false,
+    },
+    {
+      title: 'Jonanek jidlo',
+      cena: 1500,
+      imgs: [{ img1: '/assets/ZsPlechuJidlo1.webp', img2: '/assets/ZsPlechuJidlo2.webp' }],
+      owned: false,
+      CBuy: false,
+    },
+    {
+      title: 'Jonanek závodník',
+      cena: 10000,
+      imgs: [{ img1: '/assets/zavodnik1.webp', img2: '/assets/zavodnik2.webp' }],
+      owned: false,
+      CBuy: false,
+    },
+  ];
 }
