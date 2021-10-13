1840e5237e370e047704741f76fad26c6e09a782
Marcel <numax.cz@protonmail.ch>
2021-10-13 16:28:59 +0200

    Upraven login component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { FocusInputDirective } from './focus-input.directive';
 import { OchranaOsobnichUdajuComponent } from './ochrana-osobnich-udaju/ochrana-osobnich-udaju.component';
 import { LoginComponent } from './login/login.component';
 
+
 const routes: Routes = [
   {
     path: '',
@@ -58,7 +59,7 @@ const routes: Routes = [
     BoardComponent,
     FocusInputDirective,
     OchranaOsobnichUdajuComponent,
-    LoginComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
