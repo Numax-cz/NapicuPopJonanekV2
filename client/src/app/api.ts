50ce2e9816f462099520374454be415cb3fde2e1
Marcel <numax.cz@protonmail.ch>
2021-10-15 19:25:51 +0200

    Přidáno api

--- client/src/app/api.ts
+++ client/src/app/api.ts
@@ -1,3 +1,7 @@
-export const GlobalUpdate = "https://api.popjonanek.napicu.eu/api/update"
+export const GlobalUpdate = 'https://api.popjonanek.napicu.eu/api/update';
 export const UserNameCheck = 'https://api.popjonanek.napicu.eu/api/usernamecheck';
 export const UserRegister = 'https://api.popjonanek.napicu.eu/api/reg';
+/**
+ * Body: `email` - password reset request, sends an email and creates an entry in the PasswordReset table, code 0 = successful
+ */
+export const UserResetPass = ' https://api.popjonanek.napicu.eu/api/resetpassword';
