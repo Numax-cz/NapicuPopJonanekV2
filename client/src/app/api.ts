b432b6dc10ea423675498f9ea9ce8e787ad93fbb
Marcel <numax.cz@protonmail.ch>
2021-10-16 14:02:32 +0200

    Odstraněn komentář

--- client/src/app/api.ts
+++ client/src/app/api.ts
@@ -2,8 +2,5 @@ export const domain = 'https://api.popjonanek.napicu.eu';
 export const GlobalUpdate = `${domain}/api/update`;
 export const UserNameCheck = `${domain}/api/usernamecheck`;
 export const UserRegister = `${domain}/api/reg`;
-/**
- * Body: `email` - password reset request, sends an email and creates an entry in the PasswordReset table, code 0 = successful
- */
 export const UserResetPass = `${domain}/api/resetpassword`;
 export const UserResetPassCheck = `${domain}/api/resetpassword/check`;
