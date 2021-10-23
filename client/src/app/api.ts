b3213318b6e046844eaf525d29e25d47b2fdb5a3
Marcel <numax.cz@protonmail.ch>
2021-10-23 15:11:08 +0200

    Přidán Board API

--- client/src/app/api.ts
+++ client/src/app/api.ts
@@ -4,3 +4,4 @@ export const UserNameCheck = `${domain}/api/usernamecheck`;
 export const UserRegister = `${domain}/api/reg`;
 export const UserResetPass = `${domain}/api/resetpassword`;
 export const UserResetPassCheck = `${domain}/api/resetpassword/check`;
+export const GlobalBoard = `${domain}/api/leaderboard`;
