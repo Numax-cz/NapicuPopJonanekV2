039831f58336b4ae723cecb8ddcb78cdbe675b21
Marcel <numax.cz@protonmail.ch>
2021-10-23 16:15:23 +0200

    Přidáno API pro kontrolu přihlášení

--- client/src/app/user.guard.ts
+++ client/src/app/user.guard.ts
@@ -1,15 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-
+import { UserCheck } from './api';
+declare interface ApiUserCeck {
+  loginedin: boolean;
+  username: string;
+  email: string;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserGuard implements CanActivate {
+  constructor(protected http: HttpClient) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    this.http.get<any>(UserCheck).subscribe((data: ApiUserCeck) => {});
+
     return true;
   }
-  
 }
