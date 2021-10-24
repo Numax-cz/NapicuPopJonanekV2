3e71c09c02be9356d0e21ccf9fb0ac29ffa6428a
Marcel <numax.cz@protonmail.ch>
2021-10-24 11:40:23 +0200

    Upraven styl kódu, změněn balíček pro sessiony

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const crypto = require('crypto');
-const cookieSession = require('cookie-session');
+const cookieSession = require('express-session');
 const expressRateLimit = require('express-rate-limit');
-const codes = require("./codes.json");
+const codes = require('./codes.json');
 const app = express();
 require('dotenv').config();
 
@@ -41,22 +41,23 @@ app.all('/*', function (req, res, next) {
   next();
 });
 
-
 //Login check
 app.use((req, res, next) => {
-  if (req.session.username != undefined & req.session.password != undefined) {
-    connection.query(`SELECT * FROM Users WHERE Username LIKE '${req.session.username}' AND SHA256Password LIKE '${req.session.password}' LIMIT 1`, (err, result) => {
-      if (!err) {
-        if (result.length > 0) {
-          res.locals.loginedin = true;
-          res.locals.user = result[0];
-          next();
-        } else {
-          req.session = null;
-          res.json(codes[6014]);
+  if ((req.session.username != undefined) & (req.session.password != undefined)) {
+    connection.query(
+      `SELECT * FROM Users WHERE Username LIKE '${req.session.username}' AND SHA256Password LIKE '${req.session.password}' LIMIT 1`,
+      (err, result) => {
+        if (!err) {
+          if (result.length > 0) {
+            res.locals.loginedin = true;
+            res.locals.user = result[0];
+            next();
+          } else {
+            req.session = null;
+            res.json(codes[6014]);
+          }
         }
       }
-    }
     );
   } else {
     res.locals.loginedin = false;
@@ -78,7 +79,6 @@ app.post('/api/reg', RegLimiter, (req, res) => {
     res.json(codes[6013]);
   } else if (!req.body.name || !req.body.email || !req.body.passwords.pass1 || !req.body.passwords.pass2) {
     res.json(codes[6001]);
-
   } else if (req.body.passwords.pass1 != req.body.passwords.pass2) {
     res.json(codes[6002]);
   } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)) {
@@ -87,16 +87,24 @@ app.post('/api/reg', RegLimiter, (req, res) => {
     res.json(codes[6015]);
   } else if (req.body.name.length < 4 || req.body.name.length > 15) {
     res.json(codes[6004]);
-  } else if (req.body.passwords.pass1.length < 8 || req.body.passwords.pass1.length > 40 || !/\d/.test(req.body.passwords.pass1) || !/[a-z]/.test(req.body.passwords.pass1) || !/[A-Z]/.test(req.body.passwords.pass1)) {
+  } else if (
+    req.body.passwords.pass1.length < 8 ||
+    req.body.passwords.pass1.length > 40 ||
+    !/\d/.test(req.body.passwords.pass1) ||
+    !/[a-z]/.test(req.body.passwords.pass1) ||
+    !/[A-Z]/.test(req.body.passwords.pass1)
+  ) {
     res.json(codes[6005]);
   } else {
-    connection.query(`SELECT * FROM Users WHERE Username LIKE '${req.body.name}' OR Email LIKE '${req.body.email}`,
+    connection.query(
+      `SELECT * FROM Users WHERE Username LIKE '${req.body.name}' OR Email LIKE '${req.body.email}`,
       (err, result) => {
         if (result) {
           res.json(codes[6006]);
         } else {
           const hash = crypto.createHash('sha256').update(req.body.passwords.pass1).digest('hex');
-          connection.query(`INSERT INTO Users (Username, SHA256Password, Email) VALUES ('${req.body.name}', '${hash}', '${req.body.email}')`,
+          connection.query(
+            `INSERT INTO Users (Username, SHA256Password, Email) VALUES ('${req.body.name}', '${hash}', '${req.body.email}')`,
             (err2) => {
               if (err2) {
                 res.json(codes[6007]);
@@ -125,19 +133,26 @@ app.post('/api/reg', RegLimiter, (req, res) => {
   }
 });
 
-app.post("/api/userdelete/check", (req, res) => {
+app.post('/api/userdelete/check', (req, res) => {
   if (!res.locals.loginedin) {
     res.json(codes[6014]);
   } else {
-    connection.query(`SELECT * FROM EmailCodes WHERE User_ID LIKE '${res.locals.ID}' AND CodeType LIKE '2' ORDER BY ID DESC LIMIT 1`,
+    connection.query(
+      `SELECT * FROM EmailCodes WHERE User_ID LIKE '${res.locals.ID}' AND CodeType LIKE '2' ORDER BY ID DESC LIMIT 1`,
       (err2, result) => {
-        if (result.length < 1 || new Date() - new Date(result[0].CreationTimestamp) > process.env.RESETPASSWORD_CODE_ACTIVE_TIME_MS) {
+        if (
+          result.length < 1 ||
+          new Date() - new Date(result[0].CreationTimestamp) > process.env.RESETPASSWORD_CODE_ACTIVE_TIME_MS
+        ) {
           if (result.length > 0) {
             connection.query(`DELETE FROM EmailCodes WHERE ID LIKE '${res.locals.ID}' AND CodeType LIKE '2'`);
           }
 
           const DelCode = crypto.randomBytes(8).toString('hex');
-          connection.query(`INSERT INTO EmailCodes (User_ID, CodeType, Code, CreationTimestamp) VALUES ('${res.locals.ID}', 1, '${DelCode}', '${new Date().getTime()}')`,
+          connection.query(
+            `INSERT INTO EmailCodes (User_ID, CodeType, Code, CreationTimestamp) VALUES ('${
+              res.locals.ID
+            }', 1, '${DelCode}', '${new Date().getTime()}')`,
             (err2) => {
               if (err2) {
                 res.json(codes[6007]);
@@ -169,22 +184,28 @@ app.post("/api/userdelete/check", (req, res) => {
       }
     );
   }
-})
+});
 
-
-app.post("/api/userdelete", (req, res) => {
+app.post('/api/userdelete', (req, res) => {
   if (!res.locals.loginedin) {
     res.json(codes[6014]);
   } else {
-    connection.query(`SELECT * FROM EmailCodes WHERE User_ID LIKE '${res.locals.ID}' AND CodeType LIKE '2' ORDER BY ID DESC LIMIT 1`,
+    connection.query(
+      `SELECT * FROM EmailCodes WHERE User_ID LIKE '${res.locals.ID}' AND CodeType LIKE '2' ORDER BY ID DESC LIMIT 1`,
       (err2, result) => {
-        if (result.length < 1 || new Date() - new Date(result[0].CreationTimestamp) > process.env.RESETPASSWORD_CODE_ACTIVE_TIME_MS) {
+        if (
+          result.length < 1 ||
+          new Date() - new Date(result[0].CreationTimestamp) > process.env.RESETPASSWORD_CODE_ACTIVE_TIME_MS
+        ) {
           if (result.length > 0) {
             connection.query(`DELETE FROM EmailCodes WHERE ID LIKE '${res.locals.ID}' AND CodeType LIKE '2'`);
           }
 
           const DelCode = crypto.randomBytes(8).toString('hex');
-          connection.query(`INSERT INTO EmailCodes (User_ID, CodeType, Code, CreationTimestamp) VALUES ('${res.locals.ID}', 1, '${DelCode}', '${new Date().getTime()}')`,
+          connection.query(
+            `INSERT INTO EmailCodes (User_ID, CodeType, Code, CreationTimestamp) VALUES ('${
+              res.locals.ID
+            }', 1, '${DelCode}', '${new Date().getTime()}')`,
             (err2) => {
               if (err2) {
                 res.json(codes[6007]);
@@ -216,7 +237,7 @@ app.post("/api/userdelete", (req, res) => {
       }
     );
   }
-})
+});
 
 app.post('/api/resetpassword/check', (req, res) => {
   if (res.locals.loginedin) {
@@ -224,7 +245,8 @@ app.post('/api/resetpassword/check', (req, res) => {
   } else if (!req.body.code) {
     res.json(codes[6001]);
   } else {
-    connection.query(`SELECT * FROM EmailCodes WHERE Code LIKE '${req.body.code}' AND CodeType LIKE '1' LIMIT 1`,
+    connection.query(
+      `SELECT * FROM EmailCodes WHERE Code LIKE '${req.body.code}' AND CodeType LIKE '1' LIMIT 1`,
       (err, result) => {
         if (result.length < 1) {
           res.json(codes[6011]);
@@ -241,7 +263,8 @@ app.post('/api/resetpassword/check', (req, res) => {
             res.json(codes[6002]);
           } else {
             const hash = crypto.createHash('sha256').update(req.body.heslo).digest('hex');
-            connection.query(`UPDATE Users SET SHA256Password='${hash}' WHERE ID LIKE '${result[0].User_ID}'`,
+            connection.query(
+              `UPDATE Users SET SHA256Password='${hash}' WHERE ID LIKE '${result[0].User_ID}'`,
               (err2) => {
                 if (err2) {
                   res.json(codes[6007]);
@@ -267,15 +290,25 @@ app.post('/api/resetpassword', (req, res) => {
       if (result.length < 1) {
         res.json(codes[6009]);
       } else {
-        connection.query(`SELECT * FROM EmailCodes WHERE User_ID LIKE '${result[0].ID}' AND CodeType LIKE '1' ORDER BY ID DESC LIMIT 1`,
+        connection.query(
+          `SELECT * FROM EmailCodes WHERE User_ID LIKE '${result[0].ID}' AND CodeType LIKE '1' ORDER BY ID DESC LIMIT 1`,
           (err2, result2) => {
-            if (result2.length < 1 || new Date() - new Date(result2[0].CreationTimestamp) > process.env.RESETPASSWORD_CODE_ACTIVE_TIME_MS) {
+            if (
+              result2.length < 1 ||
+              new Date() - new Date(result2[0].CreationTimestamp) >
+                process.env.RESETPASSWORD_CODE_ACTIVE_TIME_MS
+            ) {
               if (result2.length > 0) {
-                connection.query(`DELETE FROM EmailCodes WHERE ID LIKE '${result2[0].ID}' AND CodeType LIKE '1'`);
+                connection.query(
+                  `DELETE FROM EmailCodes WHERE ID LIKE '${result2[0].ID}' AND CodeType LIKE '1'`
+                );
               }
 
               const ResetCode = crypto.randomBytes(8).toString('hex');
-              connection.query(`INSERT INTO EmailCodes (User_ID, CodeType, Code, CreationTimestamp) VALUES ('${result[0].ID}', 1, '${ResetCode}', '${new Date().getTime()}')`,
+              connection.query(
+                `INSERT INTO EmailCodes (User_ID, CodeType, Code, CreationTimestamp) VALUES ('${
+                  result[0].ID
+                }', 1, '${ResetCode}', '${new Date().getTime()}')`,
                 (err2) => {
                   if (err2) {
                     res.json(codes[6007]);
@@ -307,14 +340,13 @@ app.post('/api/resetpassword', (req, res) => {
           }
         );
       }
-    }
-    );
+    });
   }
 });
 
-app.get("/api/69", (req, res) => {
-  res.send("");
-})
+app.get('/api/69', (req, res) => {
+  res.send('');
+});
 
 app.post('/api/log', (req, res) => {
   if (res.locals.loginedin) {
@@ -323,7 +355,8 @@ app.post('/api/log', (req, res) => {
     res.json(codes[6001]);
   } else {
     const hash = crypto.createHash('sha256').update(req.body.heslo).digest('hex');
-    connection.query(`SELECT * FROM Users WHERE (Username LIKE '${req.body.EmailUsername}' OR Email LIKE '${req.body.EmailUsername}') AND SHA256Password LIKE '${hash}' LIMIT 1`,
+    connection.query(
+      `SELECT * FROM Users WHERE (Username LIKE '${req.body.EmailUsername}' OR Email LIKE '${req.body.EmailUsername}') AND SHA256Password LIKE '${hash}' LIMIT 1`,
       (err, result) => {
         if (err) {
           res.json(codes[6007]);
@@ -345,18 +378,17 @@ app.get('/api/usercheck', (req, res) => {
   } else {
     res.json({ loginedin: true, username: res.locals.user.Username, email: res.locals.user.Email });
   }
-})
+});
 
 app.get('/api/leaderboard', (req, res) => {
   connection.query(`SELECT * FROM Users ORDER BY Clicks DESC LIMIT 10`, (err, result) => {
     const leaderboard = [];
-    result.forEach(user => {
+    result.forEach((user) => {
       leaderboard.push({ user: user.Username, count: user.Clicks.toString() });
     });
     res.json(leaderboard);
-  })
+  });
 });
-
 
 app.post('/api/update', async (req, res) => {
   //Počet kliků, co naklikal
@@ -369,7 +401,8 @@ app.post('/api/update', async (req, res) => {
   let UserClicks = -1;
   if (res.locals.loginedin) {
     const updatnuteKliky = result[0].Clicks + ClickCounter;
-    connection.query(`UPDATE Users SET Clicks='${updatnuteKliky}' WHERE Username LIKE '${req.session.username}'`,
+    connection.query(
+      `UPDATE Users SET Clicks='${updatnuteKliky}' WHERE Username LIKE '${req.session.username}'`,
       (err) => {
         if (err) {
           res.json(codes[6007]);
